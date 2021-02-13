9f05ff59ef330a01c327776eb8bfd92b3e62e1ef
Matt Deeds <mdeeds@gmail.com>
2021-02-13 09:41:56 -0800

    Allow hiding game fram

--- ts/gameFrame.ts
+++ ts/gameFrame.ts
@@ -1,15 +1,34 @@
 export class GameFrame {
   private iFrame: HTMLIFrameElement;
+  private source = "";
   constructor() {
     this.iFrame = document.createElement('iframe');
     this.iFrame.id = "GameFrame";
     this.iFrame.allow = "allow-scripts";
-    this.iFrame.src = "about:blank";
+    this.iFrame.hidden = false;
+    this.setContent(this.source);
     const body = document.getElementsByTagName('body')[0];
     body.appendChild(this.iFrame);
   }
 
+  show() {
+    this.uploadContent(this.source);
+    this.iFrame.hidden = false;
+  }
+
+  hide() {
+    this.iFrame.hidden = true;
+    this.uploadContent("");
+  }
+
   setContent(html: string) {
+    this.source = html;
+    if (!this.iFrame.hidden) {
+      this.uploadContent(html);
+    }
+  }
+
+  private uploadContent(html: string) {
     const dataUrl = `data:text/html;base64,${btoa(html)}`;
     this.iFrame.src = dataUrl;
   }
